0437ec7fae31741b2607d534adc47d56cbd5c1ea
unknown <maroonever96@gmail.com>
2024-07-31 10:49:09 +0900

    copy and paste sequelize directory

---
+++ day22/sequelize/models/comment.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+
+class Comment extends Sequelize.Model {
+  static initiate(sequelize) {
+    Comment.init(
+      {
+        comment: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: true,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        modelName: "Comment",
+        tableName: "comments",
+        paranoid: false,
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    // 반대의 입장에서는 belongsTo로 표현 (댓글.belongsTo(사용자))
+    // belongsTo가 있는 테이블에 컬럼이 생김 (댓글 테이블에 commenter 컬럼)
+    db.Comment.belongsTo(db.User, { foreignKey: "commenter", targetKey: "id" });
+  }
+}
+
+module.exports = Comment;
---
+++ day22/sequelize/models/user.js
@@ -0,0 +1,51 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
+        married: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  // users 모델과 comments 모델 간의 관계를 정의
+  // 1:N 관계 (사용자 한 명이 댓글 여러 개 작성)
+  // 시퀄라이즈는 1:N 관계를 hasMany로 표현 (사용자.hasMany(댓글))
+  static associate(db) {
+    db.User.hasMany(db.Comment, { foreignKey: "commenter", sourceKey: "id" });
+  }
+}
+
+module.exports = User;
