4e67c03a5fa0b79ce7a7073f07a7ca2e46abfd6b
unknown <maroonever96@gmail.com>
2024-08-01 12:30:46 +0900

    kakao login router

--- day23/unemployed_community_back_end/routes/auth.js
+++ day23/unemployed_community_back_end/routes/auth.js
@@ -9,4 +9,18 @@ router.get("/login", isNotLoggedIn, login);
 
 router.get("/logout", isLoggedIn, logout);
 
+// GET /auth/kakao
+router.get("/kakao", passport.authenticate("kakao"));
+
+// GET /auth/kakao/callback
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/?loginError=카카오로그인 실패",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
 module.exports = router;
