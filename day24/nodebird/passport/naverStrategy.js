8e55806b23005d0568331ba04391de3ce18621f7
unknown <maroonever96@gmail.com>
2024-08-05 19:48:34 +0900

    add naver login except naver logout

--- day24/nodebird/passport/naverStrategy.js
+++ day24/nodebird/passport/naverStrategy.js
@@ -1,29 +1,30 @@
 const passport = require("passport");
-const KakaoStrategy = require("passport-kakao").Strategy;
+const NaverStrategy = require("passport-naver").Strategy;
 
 const User = require("../models/user");
 
 module.exports = () => {
   passport.use(
-    new KakaoStrategy(
+    new NaverStrategy(
       {
-        clientID: process.env.KAKAO_ID,
-        callbackURL: "/auth/kakao/callback",
+        clientID: process.env.NAVER_CLIENT_ID,
+        clientSecret: process.env.NAVER_CLIENT_SECRET,
+        callbackURL: "/auth/naver/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log("kakao profile", profile);
+        console.log("naver profile", profile);
         try {
           const exUser = await User.findOne({
-            where: { snsId: profile.id, provider: "kakao" },
+            where: { snsId: profile.id.substr(0, 10), provider: "naver" },
           });
           if (exUser) {
             done(null, exUser);
           } else {
             const newUser = await User.create({
-              email: profile._json?.kakao_account?.email,
+              email: profile.emails[0].value,
               nick: profile.displayName,
-              snsId: profile.id,
-              provider: "kakao",
+              snsId: profile.id.substr(0, 10),
+              provider: "naver",
             });
             done(null, newUser);
           }
