c45b6f04c5a7281045fbb928898f957fbb54723d
unknown <maroonever96@gmail.com>
2024-08-05 11:27:33 +0900

    add renderHashtag function

--- day24/unemployed_community_back_end/controllers/page.js
+++ day24/unemployed_community_back_end/controllers/page.js
@@ -1,3 +1,5 @@
+const { User, Post, Hashtag } = require("../models");
+
 exports.renderMain = async (req, res, next) => {
   try {
     const posts = await Post.findAll({
@@ -14,5 +16,30 @@ exports.renderMain = async (req, res, next) => {
   } catch (error) {
     console.error(error);
     next(error);
+  }
+};
+
+exports.renderHashtag = async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: { title: query },
+    });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render("main", {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 };
