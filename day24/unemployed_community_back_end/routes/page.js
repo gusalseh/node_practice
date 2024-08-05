4e3deca6aedcfa27a7c429a3bf8be9ce3204ed96
unknown <maroonever96@gmail.com>
2024-08-05 11:50:07 +0900

    add hashtag router in page.js and update all routes.use

--- day24/unemployed_community_back_end/routes/page.js
+++ day24/unemployed_community_back_end/routes/page.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const { renderMain, renderHashtag } = require("../controllers/page");
+
 const router = express.Router();
-const { renderMain } = require("../controllers/page");
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
-  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
-  res.locals.followingIdList = req.user
-    ? req.user.Followings.map((f) => f.id)
-    : [];
+  res.locals.followerCount = req.user?.Followers?.length || 0;
+  res.locals.followingCount = req.user?.Followings?.length || 0;
+  res.locals.followingIdList = req.user?.Followings?.map((f) => f.id) || [];
   next();
 });
 
 router.get("/", renderMain);
+
+router.get("/hashtag", renderHashtag);
 
 module.exports = router;
